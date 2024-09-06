58dd4c9914ef44072d02c49b92471ab71a1fc21e
Ahmed <ahmedttaarek@gmail.com>
2024-09-07 00:20:42 +0300

    updates

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-#products/urls
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('', views.welcome_view, name='welcome'),  # Path for welcome view
+    path('products/', views.product_list, name='product_list'),  # Path for product list view
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),  # Path for product detail view
+    path('login/', views.login_view, name='login'),  # Path for login view
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import Product
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
+def welcome_view(request):
+    return render(request, 'welcome.html')
 
 def product_list(request):
     """View to display a list of all products."""
@@ -10,3 +16,16 @@ def product_detail(request, pk):
     """View to display details of a specific product."""
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_detail.html', {'product': product})
+
+
+def login_view(request):
+    """View to handle user login."""
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('welcome')  # Redirect to a named URL after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
