4f2bfedeb00a1686a20d4c35f04e3d743ec2753b
Ahmed <ahmedttaarek@gmail.com>
2024-09-07 00:08:17 +0300

    updates

--- products/views.py
+++ products/views.py
@@ -1,19 +1,12 @@
-#Define views to handle requests and return responses in views.py:
-
-
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
-print(Product.objects.all()) 
-
-def product_list(request): # product
-    products = Product.objects.all()
-    return render(request, 'product_list.html', {'products':products})
+def product_list(request):
+    """View to display a list of all products."""
+    products = Product.objects.filter(active=True)  
+    return render(request, 'product_list.html', {'products': products})
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
-    print(product)
+    """View to display details of a specific product."""
+    product = get_object_or_404(Product, pk=pk)
     return render(request, 'product_detail.html', {'product': product})
-
-
-
